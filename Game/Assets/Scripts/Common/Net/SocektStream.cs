19779a29d5aaf679b172138b45c6f7299dc05d53
chris <chrislue1986@hotmail.com>
2014-11-05 16:42:47 +0800

    网络模块添加

---
+++ Game/Assets/Scripts/Common/Net/SocektStream.cs
@@ -0,0 +1,154 @@
+﻿// .Net Lib
+using System;
+using System.Net;
+using System.Linq;
+using System.Net.Sockets;
+
+// Unity Lib
+using UnityEngine;
+
+namespace Game.Common
+{
+    public interface ISocketStream
+    {
+        int UnInit();
+        int Close();
+        int CheckCanSend(int nMilliseconds);
+        int CheckCanRecv(int nMilliseconds);
+        int Send(ref byte[] data, int nMilliseconds);
+        int Recv(ref byte[] data, int nMilliseconds);
+
+        string GetEndPointIp();
+        int    GetEndPointPort();
+    }
+
+    public class SocektStream : ISocketStream
+    {
+        public SocektStream()
+        {
+        }
+
+        public int Init(Socket s, IPEndPoint ipEndPoint)
+        {
+            if (m_bInitFlag)
+            {
+                Debug.LogError("[Socket Error] SocektStream can't be initialized twice!");
+                return 0;
+            }
+
+            if (null == s)
+            {
+                Debug.LogError("[Socket Error] SocektStream can't take over a null socket!");
+                return 0;
+            }
+
+            m_hRemoteSocket = s;
+            m_ipEndPoint    = ipEndPoint;
+            m_bInitFlag     = true;
+
+            return 1;
+        }
+
+        public int UnInit()
+        {
+            int nRetCode = 0;
+
+            if (!m_bInitFlag)
+            {
+                Debug.LogError("[Socket Error] SocektStream can't be uninitialized twice!");
+                return 0;
+            }
+
+            nRetCode = SocketWrapper.CloseSocket(ref m_hRemoteSocket);
+            if (1 != nRetCode)
+            {
+                Debug.LogError("[Socket Error] SocektStream close socket failed!");
+                return 0;
+            }
+
+            m_bInitFlag     = false;
+            m_hRemoteSocket = null;
+
+            return 1;
+        }
+
+        public int Close()
+        {
+            int nRetCode = SocketWrapper.CloseSocket(ref m_hRemoteSocket);
+            m_hRemoteSocket = null;
+            return nRetCode;
+        }
+
+        public int CheckCanSend(int nMilliseconds)
+        {
+            return SocketWrapper.CheckCanSend(m_hRemoteSocket, nMilliseconds);
+        }
+        public int CheckCanRecv(int nMilliseconds)
+        {
+            return SocketWrapper.CheckCanRecv(m_hRemoteSocket, nMilliseconds);
+        }
+
+        public int Send(ref byte[] data, int nMilliseconds)
+        {
+            short  nDataBytes = (short)(data.Length + sizeof(short));
+            byte[] header     = BitConverter.GetBytes(nDataBytes);
+            byte[] package    = header.Concat(data).ToArray();
+
+            return SocketWrapper.CheckSendSocketData(m_hRemoteSocket, package, (uint)package.Length, nMilliseconds);
+        }
+        public int Recv(ref byte[] data, int nMilliseconds)
+        {
+            int   nRetCode   = 0;
+            uint  uRecvBytes = 0;
+            short uDataBytes = 0;
+
+            // 收取包头
+            byte[] header = new byte[2];
+            nRetCode = SocketWrapper.CheckRecvSocketData(m_hRemoteSocket, header, 2, ref uRecvBytes, nMilliseconds);
+            if (-1 == nRetCode)
+                return -1;
+
+            if (0 == nRetCode)
+                return 0;
+
+            if (uRecvBytes < 2)
+                return -1;
+
+            uDataBytes = BitConverter.ToInt16(header, 0);
+            uDataBytes -= 2;
+
+            if (uDataBytes < 0)
+                return -1;
+
+            // 收取包体
+            data = new byte[uDataBytes];
+            nRetCode = SocketWrapper.CheckRecvSocketData(m_hRemoteSocket, data, (uint)uDataBytes, ref uRecvBytes, nMilliseconds);
+            if (nRetCode <= 0)                                          // error - 无缓冲接收必须成功
+                return -1;
+
+            if (uRecvBytes < uDataBytes)                                // error - 无缓冲接收必须成功
+                return -1;
+
+            return 1;
+        }
+        public string GetEndPointIp()
+        {
+            return m_ipEndPoint.Address.ToString();
+        }
+        public int GetEndPointPort()
+        {
+            return m_ipEndPoint.Port;
+        }
+
+        protected bool       m_bInitFlag     = false;
+        protected Socket     m_hRemoteSocket = null;
+        protected IPEndPoint m_ipEndPoint    = null;
+    }
+
+    //public class BufferedSocketStream : ISocketStream
+    //{
+    //    public BufferedSocketStream()
+    //    {
+    //    }
+    //}
+}
---
+++ Game/Assets/Scripts/Common/Net/SocketConnector.cs
@@ -0,0 +1,143 @@
+﻿// .Net Lib
+using System;
+using System.Net;
+using System.Threading;
+using System.Net.Sockets;
+
+// Unity Lib
+using UnityEngine;
+
+namespace Game.Common
+{
+    public interface ISocketConnector
+    {
+        ISocketStream Connect(string    ip, int port, int nMilliseconds);
+        ISocketStream Connect(IPAddress ip, int port, int nMilliseconds);
+    }
+
+    public class SocketConnector : ISocketConnector
+    {
+        public SocketConnector()
+        {
+
+        }
+        public ISocketStream Connect(string ip, int port,int nMilliseconds)
+        {
+            return Connect(IPAddress.Parse(ip), port, nMilliseconds);
+        }
+        public ISocketStream Connect(IPAddress ip, int port, int nMilliseconds)
+        {
+            int        nRetCode   = 0;
+            Socket     s          = SocketWrapper.CreateTcpSocket();
+            IPEndPoint ipEndPoint = new IPEndPoint(ip, port);
+
+            try
+            {
+                s.Connect(ipEndPoint);
+            }
+            catch(SocketException e)
+            {
+                int    nErrorCode = e.NativeErrorCode;
+                string sErrorDesc = SocketWrapper.SocketErrCode2Desc(nErrorCode);
+                Debug.LogErrorFormat("[SocketConnector] connect to server failed! ErrorCode : {0}, ErrorDesc : {1}!", nErrorCode, sErrorDesc);
+                SocketWrapper.CloseSocket(ref s);
+                return null;
+            }
+
+            bool bBlockingState = s.Blocking;
+            nRetCode = SocketWrapper.CheckCanSend(s, nMilliseconds);
+            if (1 != nRetCode)
+            {
+                SocketWrapper.CloseSocket(ref s);
+                return null;
+            }
+            s.Blocking = bBlockingState;
+
+            if (!s.Connected)
+            {
+                SocketWrapper.CloseSocket(ref s);
+                return null;
+            }
+
+            SocektStream ss = new SocektStream();
+            nRetCode = ss.Init(s, ipEndPoint);
+            if (1 != nRetCode)
+            {
+                SocketWrapper.CloseSocket(ref s);
+                return null;
+            }
+
+            return ss;
+        }
+    }
+
+    public class AsyncSocketConnector : ISocketConnector
+    {
+        public AsyncSocketConnector()
+        {
+        }
+        public ISocketStream Connect(string ip, int port,int nMilliseconds)
+        {
+            return Connect(IPAddress.Parse(ip), port, nMilliseconds);
+        }
+        public ISocketStream Connect(IPAddress ip, int port, int nMilliseconds)
+        {
+            int        nRetCode   = 0;
+            Socket     s          = SocketWrapper.CreateTcpSocket();
+            IPEndPoint ipEndPoint = new IPEndPoint(ip, port);
+
+            try
+            {
+                m_AllDone.Reset();                                      // 设置无信号
+                s.BeginConnect(ipEndPoint, new AsyncCallback(ConnectCallback), s);
+                bool bWaitResult = m_AllDone.WaitOne(nMilliseconds);    // 等待信号：回调函数设置有信号，这里认为成功
+
+                if (!bWaitResult || !s.Connected)
+                {
+                    Debug.LogErrorFormat("[AsyncSocketConnector] connect to server timeout or failed!");
+                    SocketWrapper.CloseSocket(ref s);
+                    return null;
+                }
+            }
+            catch(SocketException e)
+            {
+                int    nErrorCode = e.NativeErrorCode;
+                string sErrorDesc = SocketWrapper.SocketErrCode2Desc(nErrorCode);
+                Debug.LogErrorFormat("[AsyncSocketConnector] connect to server failed! ErrorCode : {0}, ErrorDesc : {1}!", nErrorCode, sErrorDesc);
+                SocketWrapper.CloseSocket(ref s);
+                return null;
+            }
+
+            SocektStream ss = new SocektStream();
+            nRetCode = ss.Init(s, ipEndPoint);
+            if (1 != nRetCode)
+            {
+                SocketWrapper.CloseSocket(ref s);
+                return null;
+            }
+
+            return ss;
+        }
+
+        private void ConnectCallback(IAsyncResult callback)
+        { // 该函数成功或者失败都会被系统调用
+            Socket s = callback.AsyncState as Socket;
+
+            try
+            { // 如果建立连接失败将抛出异常
+                s.EndConnect(callback);
+            }
+            catch(SocketException e)
+            {
+                int    nErrorCode = e.NativeErrorCode;
+                string sErrorDesc = SocketWrapper.SocketErrCode2Desc(nErrorCode);
+                Debug.LogErrorFormat("[AsyncSocketConnector] connect to server failed! ErrorCode : {0}, ErrorDesc : {1}!", nErrorCode, sErrorDesc);
+            }
+
+            // 放行Connect等待事件
+            m_AllDone.Set();
+        }
+
+        private static ManualResetEvent m_AllDone = new ManualResetEvent(false);
+    }
+}
