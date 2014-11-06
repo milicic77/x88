adbbd4af7f70a77aa7e63f174e1ad5db8fc2c3ac
chris <chrislue1986@hotmail.com>
2014-11-06 18:28:55 +0800

    增加meta文件

--- Game/Assets/Scripts/Common/Pattern/Singleton.cs
+++ Game/Assets/Scripts/Common/Pattern/Singleton.cs
@@ -1,26 +1,32 @@
 ﻿
 namespace Game.Common
 {
-    /**
-     *   Use:
-     *   
-     *      Response.Write(Singleton<Test>.Instance().Time);  ...
-     */
+    /*------------------------------------------------------------------
+      Template    : Singleton<T>
+      Description : 单例模式实现模板。
+      Usage       : Test instance = Singleton<Test>.Instance();
+      Tips        : where T : A     - 表示类型T继承于A或A本身
+                    where T : new() - 指明创建T实例时应该使用的构造函数
+    --------------------------------------------------------------------*/
     public class Singleton<T> where T : new()
     {
-        public Singleton() { }
+        public Singleton()
+        {
+        }
 
         public static T Instance()
         {
-            return SingletonCreator.instance;
+            return SingletonCreator._instance;
         }
 
         class SingletonCreator
         {
-            // Explicit static constructor to tell C# compiler
-            // not to mark type as beforefieldinit
-            static SingletonCreator() { }
-            internal static T instance = new T();
+            // 类静态构造函数在给定程序域中至多执行一次，只有创建类的实例或引用类的任何静态成员，才激发静态构造函数
+            static SingletonCreator()
+            {
+            }
+
+            internal static T _instance = new T();
         }
     }
 }
