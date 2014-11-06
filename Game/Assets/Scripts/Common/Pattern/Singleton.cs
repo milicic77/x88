1056148f4ed1c0ff432d5d0a244b1dbbc01f0bc4
chris <chrislue1986@hotmail.com>
2014-11-06 19:43:44 +0800

    单例模式修改

--- Game/Assets/Scripts/Common/Pattern/Singleton.cs
+++ Game/Assets/Scripts/Common/Pattern/Singleton.cs
@@ -6,14 +6,10 @@ namespace Game.Common
       Description : 单例模式实现模板。
       Usage       : Test instance = Singleton<Test>.Instance();
       Tips        : where T : A     - 表示类型T继承于A或A本身
-                    where T : new() - 指明创建T实例时应该使用的构造函数
+                    where T : new() - 指明创建T实例时应该使用无参构造函数
     --------------------------------------------------------------------*/
     public class Singleton<T> where T : new()
     {
-        public Singleton()
-        {
-        }
-
         public static T Instance()
         {
             return SingletonCreator._instance;
@@ -21,12 +17,14 @@ namespace Game.Common
 
         class SingletonCreator
         {
-            // 类静态构造函数在给定程序域中至多执行一次，只有创建类的实例或引用类的任何静态成员，才激发静态构造函数
             static SingletonCreator()
-            {
+            { // 类静态构造函数 : 程序域中至多执行一次。执行条件：创建类的实例或引用类的任何静态成员
+                _instance = new T();                                    // 第一次访问才实例化
             }
 
-            internal static T _instance = new T();
+            // internal   : 只有在同一程序集的文件中才可访问
+            // default(T) : T为引用类型返回null，T为数值类型返回0
+            internal static T _instance = default(T);
         }
     }
 }
