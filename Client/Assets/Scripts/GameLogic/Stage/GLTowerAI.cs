7a846bea3d94930e64d83face08dcd9483b44d60
chris <chrislue1986@hotmail.com>
2014-11-15 12:09:15 +0800

    炮塔AI重构第一波

--- Client/Assets/Scripts/GameLogic/Stage/GLTowerAI.cs
+++ Client/Assets/Scripts/GameLogic/Stage/GLTowerAI.cs
@@ -290,11 +290,176 @@ namespace Game.GameLogic
         }
     }
 
+    class GLTowerAITemp_2 : IGLTowerAITemp
+    {
+        class Action_LockTarget : BTActionNode
+        {
+            public override void Activate()
+            {
+                GLTower tower = m_Owner.Blackboard.GetData("self") as GLTower;
+                if (null == tower)
+                { // 炮塔对象为空，锁定失败
+                    m_State = BTTaskState.FAILURE;
+                    return;
+                }
+
+                List<GLNpc> enemies = GameWorld.Instance().Stage.NpcList;
+                if (enemies.Count <= 0)
+                { // 敌人序列为空，锁定失败
+                    m_State = BTTaskState.FAILURE;
+                    return;
+                }
+
+                for (int i = 0; i < enemies.Count; i++)
+                { // 查找射程范围中一个敌人
+                    GLNpc npc = enemies[i];
+                    bool bIsInFireRange = tower.IsInFireRange(npc);
+
+                    if (bIsInFireRange)
+                    {
+                        tower.Target = npc;
+                        m_State = BTTaskState.SUCCESS;
+                        return;
+                    }
+                }
+
+                // 锁定失败
+                m_State = BTTaskState.FAILURE;
+            }
+        }
+        class Action_AttackTarget : BTActionNode
+        {
+            public override void Activate()
+            {
+                GLTower tower = m_Owner.Blackboard.GetData("self") as GLTower;
+                if (null == tower)
+                { // 炮塔对象为空，攻击失败
+                    m_State = BTTaskState.FAILURE;
+                    return;
+                }
+
+                if (null == tower.Target)
+                { // 敌人对象为空，攻击失败
+                    m_State = BTTaskState.FAILURE;
+                    return;
+                }
+
+                GLNpc target = tower.Target as GLNpc;
+                List<GLNpc> enemies = GameWorld.Instance().Stage.NpcList;
+                if (!enemies.Contains(target))
+                { // 敌人经死亡，则攻击失败，序列执行失败
+                    tower.Target = null;
+                    m_State = BTTaskState.FAILURE;
+                    return;
+                }
+
+                // 判断射程
+                bool bIsInFireRange = tower.IsInFireRange(target);
+                if (!bIsInFireRange)
+                { // 超出射程，则攻击失败，序列执行失败
+                    tower.Target = null;
+                    m_State = BTTaskState.FAILURE;
+                    return;
+                }
+
+                // 攻击成功
+                Vector2 towerPos        = new Vector2(RepresentCommon.LogicX2WorldX(tower.LogicX), RepresentCommon.LogicY2WorldY(tower.LogicY));
+                Vector2 targetPos       = new Vector2(RepresentCommon.LogicX2WorldX(target.GetLogicCenterX()), RepresentCommon.LogicY2WorldY(target.GetLogicCenterY()));
+                Vector2 bulletDirection = targetPos - towerPos;                         // 炮塔到目标的方向向量
+                Vector2 bulletPosition  = new Vector2(RepresentCommon.LogicX2WorldX(tower.LogicX), RepresentCommon.LogicY2WorldY(tower.LogicY));
+                tower.Attack(bulletDirection, bulletPosition);
+                m_State = BTTaskState.SUCCESS;
+            }
+        }
+        class Condition_LockTarget : BTConditionNode
+        {
+            public override void Activate()
+            {
+                GLTower tower = m_Owner.Blackboard.GetData("self") as GLTower;
+                if (null == tower)
+                { // 炮塔对象为空，条件失败
+                    m_State = BTTaskState.FAILURE;
+                    return;
+                }
+
+                if (null != tower.Target)
+                { // 已经锁定目标，条件失败
+                    m_State = BTTaskState.FAILURE;
+                    return;
+                }
+
+                // 未锁定目标，条件成功
+                m_State = BTTaskState.SUCCESS;
+            }
+        }
+        class Condition_AttackTarget : BTConditionNode
+        {
+            public override void Activate()
+            {
+                GLTower tower = m_Owner.Blackboard.GetData("self") as GLTower;
+                if (null == tower)
+                { // 炮塔对象为空，条件失败
+                    m_State = BTTaskState.FAILURE;
+                    return;
+                }
+
+                if (null == tower.Target)
+                { // 还未锁定目标，条件失败
+                    m_State = BTTaskState.FAILURE;
+                    return;
+                }
+
+                // 已经锁定目标，条件成功
+                m_State = BTTaskState.SUCCESS;
+            }
+        }
+
+        public BehaviourTree Create(object self)
+        {
+            BehaviourTree         towerAI = new BehaviourTree();
+            BTNonPrioritySelector aiRoot  = new BTNonPrioritySelector();
+
+            aiRoot.Owner = towerAI;
+            aiRoot.Name  = "炮塔AI";
+
+            towerAI.RootNode = aiRoot;
+            towerAI.Blackboard.AddData("self", self);
+
+            // 锁定目标序列构建
+            BTSequenceNode       lockTarget = new BTSequenceNode();
+            Condition_LockTarget lockCond   = new Condition_LockTarget();
+            Action_LockTarget    lockAction = new Action_LockTarget();
+
+            lockTarget.Name = "[锁定目标序列]";
+            lockCond.Name   = "[锁定目标序列] - 条件节点";
+            lockAction.Name = "[锁定目标序列] - 动作节点";
+
+            aiRoot.AddNode(lockTarget);
+            lockTarget.AddCond(lockCond);
+            lockTarget.AddNode(lockAction);
+
+            // 攻击目标序列构建
+            BTSequenceNode         attackTarget = new BTSequenceNode();
+            Condition_AttackTarget attackCond   = new Condition_AttackTarget();
+            Action_AttackTarget    attackAction = new Action_AttackTarget();
+
+            attackTarget.Name = "[攻击目标序列]";
+            attackCond.Name   = "[攻击目标序列] - 条件节点";
+            attackAction.Name = "[攻击目标序列] - 攻击目标动作节点";
+
+            aiRoot.AddNode(attackTarget);
+            attackTarget.AddCond(attackCond);
+            attackTarget.AddNode(attackAction);
+
+            return towerAI;
+        }
+    }
     public class GLTowerAI
     {
         private static Dictionary<int, IGLTowerAITemp> m_TowerAITempDict = new Dictionary<int, IGLTowerAITemp>()
         {
             {1, new GLTowerAITemp_1()},
+            {2, new GLTowerAITemp_2()},
         };
 
         public static BehaviourTree Create(int nAITempId, object self)
